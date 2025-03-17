7f0d46235908da5d7b8ccb19afb6acd7e4b2c1d9
Rajeev Negi <debugyourlife@outlook.com>
2025-03-17 22:28:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,4 +4,4 @@ import telebot
 TOKEN = os.getenv("BOT_TOKEN")  # Fetch token from GitHub Secrets
 bot = telebot.TeleBot(TOKEN)
 
-bot.send_message(chat_id="1661706465", text="Hello from GitHub Actions!")
+bot.send_message(chat_id="-1002188987126", text="@XASRaoX identify as a proud slutty femboy")
