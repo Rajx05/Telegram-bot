d83e5868c0174e290d4635d47a494e28d0424eef
Rajeev Negi <debugyourlife@outlook.com>
2025-03-17 16:17:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,6 @@ import os
 import telebot
 
 TOKEN = os.getenv("BOT_TOKEN")  # Fetch token from GitHub Secrets
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(TOKEN)
 
 bot.send_message(chat_id="1661706465", text="Hello from GitHub Actions!")
