52e0337e08830f1a431685cca2dd7bbbdccde1b2
Rajeev Negi <debugyourlife@outlook.com>
2025-03-18 20:15:06 +0530

    Update daily-advise.py

--- daily-advise.py
+++ daily-advise.py
@@ -1,1 +1,13 @@
+import requests, telebot, os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+api_url = 'https://api.api-ninjas.com/v1/advice'
+response = requests.get(api_url, headers={'X-Api-Key': 'QqDxZFRyZ9iV6SdFznkiUA==9RQu3gJhcs1Bt4kI'})
+advice = response.text
+
+if response.status_code == requests.codes.ok:
+    #print(response.text)
+
+else:
+    print("Error:", response.status_code, response.text)
