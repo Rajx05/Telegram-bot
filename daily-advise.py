36d6b650b2813b8736ca79f63fe24d3efb8da4e2
Rajeev Negi <debugyourlife@outlook.com>
2025-03-18 20:53:38 +0530

    Update daily-advise.py

--- daily-advise.py
+++ daily-advise.py
@@ -1,15 +1,14 @@
-import requests, telebot, os
+import requests, telebot, os, json
 
 TOKEN = os.getenv("BOT_TOKEN")
 ID = os.getenv("CHAT_ID")
 bot = telebot.TeleBot(TOKEN)
 api_url = 'https://api.api-ninjas.com/v1/advice'
 response = requests.get(api_url, headers={'X-Api-Key': 'QqDxZFRyZ9iV6SdFznkiUA==9RQu3gJhcs1Bt4kI'})
-advice = response.text
 
 if response.status_code == requests.codes.ok:
     #print(response.text)
-    bot.send_message(ID,f"Todays Life Advise:\n->{advice})")
+    bot.send_message(ID,f"Todays Life Advise:\n->{response.json()["advice"]})")
 
 else:
     print("Error:", response.status_code, response.text)
